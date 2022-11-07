4f2abdaebd5a307e5ff129a58c950e6876000347
Orcrax <matthieu.rebate@gmail.com>
2022-11-07 13:48:15 +0100

    adding searchbar

--- config.php
+++ config.php
@@ -6,7 +6,9 @@ try{
     echo 'Erreur : '. $e->getMessage();
     die(); 
 }
+
 $booksQuery = $pdo->query("SELECT book.*, author.fullname FROM book LEFT JOIN author ON author.id=book.author_id");
 $books = $booksQuery->fetchAll(PDO::FETCH_ASSOC);
 $authorsQuery = $pdo->query("SELECT * FROM author");
 $authors = $authorsQuery->fetchAll(PDO::FETCH_ASSOC);
+$searchinbooks = $pdo->query("SELECT book.*, author.fullname FROM book LEFT JOIN author ON author.id=book.author_id");
--- index.php
+++ index.php
@@ -5,6 +5,13 @@ $sql = 'SELECT book.*, author.fullname FROM book LEFT JOIN author ON author.id=b
 $query = $pdo->prepare($sql);
 $query->execute();
 $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+/* ############SEARCHBAR############ */
+if(isset($_GET['searchbar']) && !empty($_GET['searchbar'])){
+    $search = htmlspecialchars($_GET['searchbar']);
+    $searchinbooks = $pdo->query('SELECT book.*, author.fullname FROM book LEFT JOIN author ON author.id=book.author_id WHERE book.title LIKE "%'.$search.'%" OR author.fullname LIKE "%'.$search.'%" ORDER BY id');
+}
+/* ############SEARCHBAR############ */
 ?>
 
 <!doctype html>
@@ -19,6 +26,7 @@ $result = $query->fetchAll(PDO::FETCH_ASSOC);
 <?php
     $books = $pdo->query('SELECT b.*, a.fullname FROM library.book b LEFT JOIN author a on a.id = b.author_id');
 ?>
+
     <main class="container">
         <div class="row">
             <section class="col-12">
@@ -27,29 +35,47 @@ $result = $query->fetchAll(PDO::FETCH_ASSOC);
                         echo '<div class="alert alert-danger" role="alert">'.$_SESSION['erreur'].'</div>';
                         $_SESSION['erreur'] = "";
                     }
-                ?>
+                ?>                
                 <h1>Bibliothèque</h1>
-                <table class="table">
-                    <thead>
-                        <th>Titre</th>
-                        <th>Auteur</th>
-                        <th></th>
-                    </thead>
+                <div class="form">
+                    <i class="fa fa-search"></i>
+                    <form method="GET">
+                        <input type="search" name="searchbar" placeholder="Rechercher un livre ou un auteur..." class="form-control form-input">
+                        <input class="btn btn-primary" type="submit" name="Rechercher">
+                    </form>
+                </div>
+    <!-- ################################################## -->
+
+    <section class="search_results">
+    <table class="table">
+        <thead>
+            <th>Titre</th>
+            <th>Auteur</th>
+            <th></th>
+        </thead>
+        <?php
+            if($searchinbooks->rowCount() > 0){
+                while($book = $searchinbooks->fetch()){
+                    ?>                                       
                     <tbody>
-                        <?php
-                        foreach($result as $book){
-                        ?>
-                            <tr>
-                                <td><?= $book['title'] ?></td>
-                                <td><?= $book['fullname'] ?></td>
-                                <td><a href="detail.php?id=<?= $book['id']?>">Détails</a>  <a href="edit.php?id=<?= $book['id']?>">Edit</a>
-                                <a href="erase.php?id=<?= $book['id']?>">Supprimer</a></td>
-                        <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
-                <a class="btn btn-primary" href="add.php">Ajouter un livre</a>
+                    <tr>
+                        <td><?= $book['title'] ?></td>
+                        <td><?= $book['fullname'] ?></td>
+                        <td><a href="detail.php?id=<?= $book['id']?>">Détails</a>  <a href="edit.php?id=<?= $book['id']?>">Edit</a>
+                        <a href="erase.php?id=<?= $book['id']?>">Supprimer</a></td>
+        <?php 
+        }
+
+        }else{
+            ?>
+            <p>Aucun livre trouvé</p>
+            <?php
+            }
+        ?>
+        </table>
+    </section>
+
+              <a class="btn btn-primary" href="add.php">Ajouter un livre</a>
             </section>
         </div>
     </main>
